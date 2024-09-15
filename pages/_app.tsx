1c107cdaaaa723329879e2a9ee851b4764ebe780
donicnu <donicnu@gmail.com>
2024-09-15 19:43:29 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("APP Component -  PAGES ROUTER");
-  return <Component {...pageProps} />;
+  // @ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
+
+  //socketio, redux ....
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  ); //app qolgan page routing file larga jonatish mantiq
 }
