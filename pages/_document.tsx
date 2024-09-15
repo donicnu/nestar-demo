f81734a27314066ac0f6368754e6f308f1b87871
donicnu <donicnu@gmail.com>
2024-09-15 18:41:08 +0900

    feat: meta tags integration on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT -  PAGES ROUTER");
+  // console.log("DOCUMENT -  PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index.follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+        {/* Search Engine Optimization - SEO */}
+        <meta
+          name="keyword"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and Sell properties anywhere anytime in South Korea. |" +
+            "Russian: Koreani lyuboy joyida lyuboy ko'chmas mulk oldi sotdisi. |" +
+            "대한민국 언제 어디서나 부동산을 사고 필 수 있씁니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
