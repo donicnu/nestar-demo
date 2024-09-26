9fa4b5344fbb09ae5ad0ede6be905246e1c05a8b
donicnu <donicnu@gmail.com>
2024-09-27 01:07:00 +0900

    feat: develop mobile version

--- libs/components/Top.tsx
+++ libs/components/Top.tsx
@@ -3,8 +3,33 @@ import { Box, Stack } from "@mui/material";
 import Link from "next/link";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import useDeviceDetect from "../hooks/useDeviceDetect";
 
 const Top = () => {
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return (
+      <Stack className={"navbar"}>
+        <Link href={"/"}>
+          <div>Home</div>
+        </Link>
+        <Link href={"/property"}>
+          <div>Properties</div>
+        </Link>
+        <Link href={"/agent"}>
+          <div>Agents</div>
+        </Link>
+        <Link href={"/community"}>
+          <div>Community</div>
+        </Link>
+
+        <Link href={"/cs"}>
+          <div>CS</div>
+        </Link>
+      </Stack>
+    );
+  }
   return (
     <Stack className={"navbar"}>
       <Stack className={"navbar-main"}>
@@ -25,7 +50,7 @@ const Top = () => {
             <Link href={"/agent"}>
               <div>Agents</div>
             </Link>
-            <Link href={"/community?articleCategory=FREE"}>
+            <Link href={"/community"}>
               <div>Community</div>
             </Link>
 
